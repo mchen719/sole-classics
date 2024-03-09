4403c348b5b1f96daa0c12798eb6e95927149ee3
Matthew Chen <m.chen71996@mail.com>
2024-03-09 10:25:47 -0500

    Cleaned up some code that was commented out

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,20 +3,6 @@ import { useState } from 'react'
 
 export const NavBar = ({ input, setInput }) => {
 
-	// const fetchData = (value) => {
-	// 	fetch('/shoes').then((response) => response.json()).then((json) => {
-	// 		const results = json.filter((item) => {
-	// 			return(
-	// 				value &&
-	// 				item && 
-	// 				item.name &&
-	// 				item.tags.toLowerCase().includes(value)
-	// 			)
-	// 		})
-	// 		setResults(results)
-	// 	})
-	// }
-
 	const handleChange = (value) => {
 		setInput(value)
 	}
